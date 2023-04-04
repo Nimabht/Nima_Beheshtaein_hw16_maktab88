fb51b1c569fab576ba71c56687bef05f71a76220
nimabht <nimabeheshtaein99@gmail.com>
2023-04-04 21:14:26 +0330

    handling search queries in getEmployees router.

--- 1-2/controllers/employees.js
+++ 1-2/controllers/employees.js
@@ -4,12 +4,48 @@ import AppError from "../utils/appError.js";
 
 export const getEmployees = async (req, res, next) => {
   const page = parseInt(req.query.page) || 1;
-  const pageSize = 6;
+  const filter = {};
+  let sort = { lastname: -1 };
+  if (req.query.province) {
+    filter.province = req.query.province;
+  }
+  if (req.query.age) {
+    const today = new Date();
+    const ageLimitDate = new Date(
+      today.getFullYear() - req.query.age,
+      today.getMonth(),
+      today.getDate()
+    );
+    filter.dateOfBirth = { $lte: ageLimitDate };
+  }
+  if (req.query.phone) {
+    filter.phoneNumber = { $size: 1 };
+  }
+  if (req.query.role) {
+    filter.roleInCompany = req.query.role;
+  }
+  if (req.query.gender) {
+    filter.gender = req.query.gender;
+  }
+  if (req.query.registeredLastWeek) {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    filter.registrationDate = { $gte: oneWeekAgo };
+  }
+  if (req.query.sortBy === "ageAsc") {
+    sort = { dateOfBirth: 1 };
+  }
 
+  // Sorting by age in descending order
+  if (req.query.sortBy === "ageDesc") {
+    sort = { dateOfBirth: -1 };
+  }
+  const pageSize = 6;
   const skipCount = (page - 1) * pageSize;
-  const count = await Employee.count();
-  const employees = await Employee.find()
-    .sort({ lastname: -1 })
+  const resEmployees = await Employee.find(filter);
+  const count = resEmployees.length;
+  const employees = await Employee.find(filter)
+    .sort(sort)
     .skip(skipCount)
     .limit(pageSize)
     .select("-__v");
